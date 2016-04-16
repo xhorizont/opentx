612f8e3e8aec67b6a00f8054625846c4bc779b1f
Andre Bernet <andre@andrebernet.ch>
2016-04-16 23:37:55 +0400

    Documentation

--- radio/src/targets/horus/pwr_driver.cpp
+++ radio/src/targets/horus/pwr_driver.cpp
@@ -98,12 +98,27 @@ void pwrResetHandler()
   __ASM volatile ("nop");
   __ASM volatile ("nop");
 
-  // Turn soft power ON now, but only if we got started because of the watchdog
-  // or software reset. If the radio was started by user pressing the power button
-  // then that button is providing power and we don't need to enable it here.
+  // We get here whether we are powering up normally, we had an unexpected reboot or we have just powered down normally.
+  // We want:
+  // - In the 2nd case, to power ON as soon as possible if an unexpected reboot happened 
+  //   (we get there running on remaining capacitor charge, soft power having been cut by the RESET).
+  // - In the 3rd case, NOT power on as that would prevent from turning the system off.
+  // - The 1st case does not need to be handled here, but will be as a result of the handling for the 3rd case, see below.
   //
-  // If we were to turn it on here indiscriminately, then the radio can go into the 
-  // power on/off loop after being powered off by the user. (issue #2790)
+  // shutdownRequest is used to handle the 3rd case. If we really powered down on purpose this will still be set to SHUTDOWN_REQUEST
+  // as we left it in pwrOff(). If however we had an unexpected reboot, it would be set to NO_SHUTDOWN_REQUEST as we set it in pwrOn().
+  // Any other value (e.g. resulting from data corruption) would also keep power on for safety, so this variable can NOT be used
+  // to detect an unexpected reboot (on a normal power on the contents of the variable are random).
+  // 
+  // shutdownReason is used to differentiate between an unexpected reboot and a normal power on. We set it to DIRTY_SHUTDOWN in pwrOn() 
+  // in anticipation of a potential reboot. Should there be one the value should be preserved and signal below that we rebooted unexpectedly.
+  // If it is NOT set to DIRTY_SHUTDOWN we likely had a normal boot and its contents are random. Due to the need to initialize it to detect a
+  // potential failure ASAP we cannot use it to determine in the firmware why we got there, it has to be buffered.
+  //
+  // powerupReason is there to cater for that, and is what is used in the firmware to decide whether we have to enter emergency mode.
+  // This variable needs to be in a RAM section that is not initialized or zeroed, since once we exit this pwrResetHandler() function the
+  // C runtime would otherwise overwrite it during program init.
+
   if (UNREQUESTED_SHUTDOWN()) {
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Pin = PWR_ON_GPIO_PIN;
